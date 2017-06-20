9e15132cdbb73e6375e79de7f16cae84892393c7
Brian Juul Andersen <mail@kree8tive.dk>
2017-06-20 21:07:07 +0200

    init

---
+++ WebApplication2/Scripts/app.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    function TestController($scope) {
+        $scope.testmethod = function (input) {
+            console.log(input);
+            $scope.returnVal = "input = " + input;
+        }
+    }
+
+    angular
+        .module('spa', [])
+        .controller('TestController', ['$scope', TestController]);
+})();
